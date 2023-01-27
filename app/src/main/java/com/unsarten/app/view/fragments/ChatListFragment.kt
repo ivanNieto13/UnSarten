870ee6e181916c54746fff16c0b61039f27a0fac
Ivan Nieto <ivannieto59@aragon.unam.mx>
2023-01-27 17:21:11 -0600

    feat: add chat list fragment + layout

---
+++ app/src/main/java/com/unsarten/app/view/fragments/ChatListFragment.kt
@@ -0,0 +1,24 @@
+package com.unsarten.app.view.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.unsarten.app.databinding.FragmentChatListBinding
+
+class ChatListFragment : Fragment() {
+    private var _binding: FragmentChatListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentChatListBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+}
